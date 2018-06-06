e9f8ffd85fce941499f69cc57122e71ee3c237f7
Tyler Denk <denk.tyler@gmail.com>
2018-06-05 21:12:14 -0400

    arrow navigation
    implemented simple up/down navigation via the keyboard arrows + enter
    using jQuery

--- SOLUTION.md
+++ SOLUTION.md
@@ -1,3 +1,18 @@
 # Solution Docs
 
 <!-- You can include documentation, additional setup instructions, notes etc. here -->
+
+Hey all. Sorry in advance for solutions being a little rocky...I haven't done too much javascript in my current role for the past ~8 months. 
+
+# Comments on the keyboard shortcuts up/down navigation
+
+1) Right now, you can begin to type and it was display up to 10 results that match the query. 
+
+2) Up/down navigation with submission on enter works as you would expect. Allows user to continue to scroll and select (similar to the default setting via mouse scroll and click, which allows you to continue to query and select multiple times).
+
+3) While navigating via up/down keys you can also scrollover with the mouse and have that takeover as the primary navigation tool. Once you scrollover, it removes the up/down navigation. In order to resume use of up/down arrows, you need to remove the mouse from the dropdown menu area.
+
+4) Being somewhat time constained I figured I'd rather go for basic functionality over perfection for the above #3. Ideally I'd use jQuery to keep track of which of the <li> is being hovered via the mouse, store that somewhere, then allow you to take over with the arrows and have it leave off from where the mouse was previously hovering. Then on mouse move, it will update the the selected <li> element..so the arrows would take over primary navigation when the mouse is stationary (and on the dropdown). This way the mouse and arrow keys can work simultaneously to optimize navigation.
+
+5) Can continue to type and edit the query after hitting enter on navigation. Need to refocus the input form though on click if you select an option via mouse click.
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Autocomplete from './Autocomplete';
 import usStates from './us-states';
 import './main.css';
+import $ from "jquery";
 
 
 // US States
@@ -8,6 +9,13 @@ const data = usStates.map(state => ({
   text: state.name,
   value: state.abbreviation
 }));
+
+// incremental variables for state and github 
+var i = -1;	 // state
+var j = -1;  // github
+
+var disableArrow = false;
+
 new Autocomplete(document.getElementById('state'), {
   data,
   onSelect: (stateCode) => {
@@ -15,10 +23,71 @@ new Autocomplete(document.getElementById('state'), {
   },
 });
 
-
-// Github Users
+//Github Users
 // new Autocomplete(document.getElementById('gh-user'), {
 //   onSelect: (ghUserId) => {
 //     console.log('selected github user id:', ghUserId);
 //   },
 // });
+
+
+
+$(".state-group input").keydown(function(e){
+	var key = e.keyCode;
+	var state = $('.result');
+	var maxResults = state.length
+
+	if ( key != 40 && key != 38 && key != 13 ) {
+		$(state).removeClass('selected');
+		i = -1;
+		return;
+	}
+	e.preventDefault();
+	switch(key){
+		case 13: // enter key
+			$.each(data, function(x,v){
+    		if ($(state[i]).text() == v['text']){
+    			console.log('selected state:', v['value']);
+	    		};
+	    	});
+	    	break;	
+		case 38: // up arrow
+			if (disableArrow == false){
+				$(state[i]).removeClass('selected');
+		    	$(state[i-1]).addClass('selected');
+		    	if (i > -1){
+		    		i--;
+		    	};
+		    	break;
+			};
+		case 40: // down arrow
+			if (disableArrow == false && maxResults > (i+1)){
+				$(state[i]).removeClass('selected');
+		    	$(state[i+1]).addClass('selected');
+		    	i++;
+		    	break;
+			};
+	    default:
+	    	break;	
+	}
+});
+
+
+// code originally taken and modified from stack overflow (https://stackoverflow.com/questions/1273566/how-do-i-check-if-the-mouse-is-over-an-element-in-jquery?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa)
+$("#state .results").mouseenter(function(){
+    clearTimeout($(this).data('timeoutId'));
+    $(this).find(".tooltip").fadeIn("slow");
+    $('.state-group .result').removeClass('selected');
+    i = -1;
+    disableArrow = true;
+}).mouseleave(function(){
+    var someElement = $(this),
+        timeoutId = setTimeout(function(){
+            someElement.find(".tooltip").fadeOut("slow");
+        }, 650);
+        disableArrow = false
+    //set the timeoutId, allowing us to clear this trigger if the mouse comes back over
+    someElement.data('timeoutId', timeoutId); 
+});
+
+
